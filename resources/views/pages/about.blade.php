0fe7d3d495a3c34e71a5a91ea05c950b17108334
Yun-Jan <yt266@njit.edu>
2018-10-13 18:14:00 -0400

    Laravel setup

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.default')
+@section('content')
+
+
+    <h1> Welcome to my About Page </h1>
+    <p>Welcome to my About Page</p>
+
+
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+@section('content')
+
+
+    <h1>Hello World Welcome to my Homepage </h1>
+    <p>Hello World Welcome to my Homepage</p>
+
+@stop
