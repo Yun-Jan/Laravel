181095587353807ebc690210a52aebccd3fad974
Yun-Jan <yt266@njit.edu>
2018-10-17 22:13:07 -0400

    Edited content

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,18 +4,23 @@
     @include('includes.head')
 </head>
 <body>
-
-<header>
+<div class="container-fluid">
     @include('includes.header')
-</header>
+    <footer class="container">
+        <div class="container">
+            <main role="main">
+                @yield('content')
+            </main>
+        </div>
 
-
-    <footer class="row">
-        @include('includes.footer')
     </footer>
-    <main role="main">
+</div>
+<main role="main">
+    @yield('content')
+</main>
+@include('includes.footer')
 
-        @yield('content')
+
 </main>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
@@ -26,5 +31,6 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
+
 </body>
 </html>
